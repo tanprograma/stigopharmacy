585a6a1e2a8e1f83854846dd9fba5d883659950a
kija <tanprograma@gmail.com>
2023-08-11 14:47:00 +0300

    new code from julyfifteen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   title: 'stigoRX',
-  medicines_url: 'https://julyfifteenapi.onrender.com/api/medicines',
-  clients_url: 'https://julyfifteenapi.onrender.com/api/clients',
-  inventories_url: 'https://julyfifteenapi.onrender.com/api/inventories',
-  stores_url: 'https://julyfifteenapi.onrender.com/api/stores',
-  units_url: 'https://julyfifteenapi.onrender.com/api/units',
-  suppliers_url: 'https://julyfifteenapi.onrender.com/api/suppliers',
+  medicines_url: 'https://stigopharmacyapi.onrender.com/api/medicines',
+  clients_url: 'https://stigopharmacyapi.onrender.com/api/clients',
+  inventories_url: 'https://stigopharmacyapi.onrender.com/api/inventories',
+  stores_url: 'https://stigopharmacyapi.onrender.com/api/stores',
+  units_url: 'https://stigopharmacyapi.onrender.com/api/units',
+  suppliers_url: 'https://stigopharmacyapi.onrender.com/api/suppliers',
 };
